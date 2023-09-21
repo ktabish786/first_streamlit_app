192af59ea0a8d5940b0ed375933df8c136c7f6ea
ktabish786 <60180684+ktabish786@users.noreply.github.com>
2023-09-21 15:15:00 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,7 +1,7 @@
 import streamlit
 import pandas
 
-streamlit.header('My Mom's new healthy dinner')
+streamlit.header("My Mom's new healthy dinner")
 streamlit.text('🥣 Omega 3 & Blueberry Oatmeal')
 streamlit.text('🥗 Kale, Spinach & Rocket Smoothie')
 streamlit.text('🐔 Hard-Boiled Free-Range Egg')
